18dbadd3bea4e57ec6ad61db68954a85485ea517
niufan <niufan_work@163.com>
2018-09-15 21:57:26 +0800

    table label modify

--- src/app/resource/resource.module.ts
+++ src/app/resource/resource.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { CdkTableModule } from '@angular/cdk/table';
 
-import { MatTableModule, MatPaginatorModule } from '@angular/material';
+import {MatTableModule, MatPaginatorModule, MAT_PAGINATOR_INTL_PROVIDER, MatPaginatorIntl} from '@angular/material';
 
 import { ResourceRoutingModule } from './resource-routing.module';
 
@@ -10,7 +10,28 @@ import { ResourceComponent } from './resource.component';
 
 import { ResourcePageComponent } from './page/resource-page.component';
 
-
+function MAT_PAGINATOR_INTL_PROVIDER_FACTORY(parentIntl: MatPaginatorIntl): MatPaginatorIntl {
+  let intl: MatPaginatorIntl;
+  if (parentIntl === undefined) {
+    intl = new MatPaginatorIntl();
+  } else {
+    intl = parentIntl;
+  }
+  intl.itemsPerPageLabel = '每页条数：';
+  intl.nextPageLabel = '下一页';
+  intl.previousPageLabel = '上一页';
+  intl.firstPageLabel = '首页';
+  intl.lastPageLabel = '尾页';
+  intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0 || pageSize === 0) {
+      return `没有数据`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize, endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return `第 ${startIndex + 1} 至 ${endIndex} 条 共 ${length} 条`;
+  };
+  return intl;
+}
 
 @NgModule({
   declarations: [
@@ -27,6 +48,13 @@ import { ResourcePageComponent } from './page/resource-page.component';
   exports: [
     ResourceComponent,
     ResourcePageComponent
+  ],
+  providers: [
+    {
+      provide: MatPaginatorIntl,
+      deps: [[new Optional(), new SkipSelf(), MatPaginatorIntl]],
+      useFactory: MAT_PAGINATOR_INTL_PROVIDER_FACTORY
+    }
   ]
 })
 export class ResourceModule { }
